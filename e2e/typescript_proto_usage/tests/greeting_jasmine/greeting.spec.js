dbaebcb7dae2a44c7fba8b546bdbfc98ee245e2c
Red Daly <reddaly@gmail.com>
2023-01-10 19:53:40 -0800

    fixup! Add new e2e workspace (copy, not yet working) for testing usage in TypeScript.

--- e2e/typescript_proto_usage/tests/greeting_jasmine/greeting.spec.js
+++ e2e/typescript_proto_usage/tests/greeting_jasmine/greeting.spec.js
@@ -1,12 +1,12 @@
 //import { GreetingRequest } from "../../greeting_pb.js"
-import { GreetingRequest } from "../../greeting_pb.mjs"
+import { GreetingRequest } from "../../greeting_pb.mjs";
 
 function say_hi() {
-    return new GreetingRequest().setGreeting("hello").getGreeting();
+  return new GreetingRequest().setGreeting("hello").getGreeting();
 }
 
 describe("lib", () => {
-    it("should say hi", () => {
-        expect(say_hi()).toBe("hi");
-    })  
-})
+  it("should say hi", () => {
+    expect(say_hi()).toBe("hi");
+  });
+});
