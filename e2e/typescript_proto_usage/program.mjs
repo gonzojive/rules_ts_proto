e0d7afa1a8f618a82349f11016b2b85a7d869433
Red Daly <reddaly@gmail.com>
2023-01-12 23:21:41 -0800

    Get esbuild working... demonstrate how deps are not managed strictly.

--- e2e/typescript_proto_usage/program.mjs
+++ e2e/typescript_proto_usage/program.mjs
@@ -1,3 +1,4 @@
-import { GreetingRequest } from "./greeting_pb.mjs";
+//import { GreetingRequest } from "./greeting_pb.mjs";
+import { Position } from "./location/location_pb.mjs";
 
-console.log("request: %s", new GreetingRequest());
+console.log("request: %s", new Position());
